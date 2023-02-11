79f46e94a304e66785649f5906c2338b616aa436
lisic4kin <lisic4kin@yandex.ru>
2023-02-11 10:30:04 +0300

    Done first lab

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import io
-import os
 import re
-import numpy as np
+from collections import Counter
 from os import walk
+import json
 
 TEXT_DIR_PATH = "/home/lisic4kin/Documents/Labs/AI/first_lab/texts"
 
 filenames = next(walk(TEXT_DIR_PATH), (None, None, []))[2]
-
+resultFileString = ""
 for fileName in filenames:
-    inputFile = io.open((TEXT_DIR_PATH + "/" + fileName), mode='r', encoding="ISO-8859-1").read()
-    print(inputFile)
+    inputFile = io.open((TEXT_DIR_PATH + "/" + fileName), mode='r', encoding="windows-1251").read()
+    resultFileString += re.sub(r'[^а-яА-Я\s]', '', inputFile) + " "
+result = dict(Counter(resultFileString.split()))
+result = sorted(result.items(), key=lambda item: item[1], reverse=True)
 
-
-# print(type(filenames))
-#
-# s = 'Hello!@#!%!#&&!*!#$#%@*+_{ world!, sdffjhsd Саня'
-# reg = re.compile('[^a-zA-Z, ^а-яА-Я]')
-# print(reg.sub('', s))
+with open(r"Result.txt", "w") as outputFile:
+    for key, value in result:
+        outputFile.write(str(key) + ":" + str(value) + "\n")
